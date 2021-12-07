e352a8c173bc3dc8757716e7d99ce3ab008aa6d8
Nabo226 <noah.bohannon2@gmail.com>
2021-12-07 12:33:36 -0500

    Training/Test files added and updated CNN code ~55% accurate

--- RCNN.py
+++ RCNN.py
@@ -16,6 +16,13 @@ import time
 import os
 import pandas as pd
 from torchvision.io import read_image
+from torchvision.datasets import ImageFolder
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision.transforms import Resize
+from torchvision.transforms import Grayscale 
+from torchvision.transforms import Compose
+import torchvision.models as models
 
 def trainNetwork(net, num_epochs, learning_rate, trainload):
     start_time = time.time()
@@ -59,8 +66,22 @@ def testNetwork(net,testload):
         test_loss += loss.item() * data.size(0)
     print(f'Testing Loss:{test_loss/len(testload)}')
     print(f'Correct Predictions: {correct}/{total}')
-            
-            
+
+    
+transform = Compose([
+    Grayscale(),
+    Resize(224),
+    ToTensor()
+    ])
+    
+dataset_train = ImageFolder(root="./archive/train", transform=transform)
+dataloader_train = DataLoader(dataset_train, batch_size=256,shuffle=True)
+#print(next(iter(dataloader_train)).shape)
+
+
+dataset_test = ImageFolder(root="./archive/test", transform=transform)
+dataloader_test = DataLoader(dataset_test, batch_size=256, shuffle=True)
+#print(next(iter(dataloader_test)).shape)
         
         
         
@@ -77,6 +98,23 @@ Alexnet = nn.Sequential(
     nn.MaxPool2d(kernel_size=3, stride=2), nn.Flatten(),
     nn.Linear(6400, 4096), nn.ReLU(), nn.Dropout(p=0.5),
     nn.Linear(4096, 4096), nn.ReLU(), nn.Dropout(p=0.5),
-    nn.Linear(4096, 10)
+    nn.Linear(4096, 7)
     )
+
+model = Alexnet
+'''
+model = nn.Sequential(nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.Sigmoid(),
+                    nn.AvgPool2d(kernel_size=2, stride=2),
+                    nn.Conv2d(6, 16, kernel_size=5), nn.Sigmoid(),
+                    nn.AvgPool2d(kernel_size=2, stride=2), nn.Flatten(),
+                    nn.Linear(16 * 5 * 5, 120), nn.Sigmoid(),
+                    nn.Linear(120, 84), nn.Sigmoid(), nn.Linear(84, 7))
+'''
+print(model)
+#print(models.AlexNet())
+
+trainNetwork(net=model, num_epochs=40, learning_rate=.1, trainload=dataloader_train)
+testNetwork(net=model, testload=dataloader_test)
+torch.save(model.state_dict(), "./model")
+
 
